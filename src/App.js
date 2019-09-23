7a0b8c96a9589e36deded0d4a4c7c0284e4ae352
hferazza <henrique.ferazza@gmail.com>
2019-09-22 21:29:27 -0300

    modified:   src/App.js 	src/assets/img/

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path="/" component={HomePage}/>
+            <Route path="/" component={HomePage}/>
             <Route path="/search" component={SearchPage}/>
           </Switch>
         </div>
